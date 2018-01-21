68d78457efc9526cd945a1ceab8e45d1b8d15c22
Adrian Sims <adriansims62781@gmail.com>
2018-01-21 23:19:52 +0000

    Add project files.

---
+++ gitmehappy.cmd
@@ -0,0 +1,8 @@
+rem echo off
+git init
+git add RjisDate/Program.cs
+git add RjisDate/App.config
+git add RjisDate/Properties/AssemblyInfo.cs
+git add RjisDate/RjisDate.csproj
+git add RjisDate.sln
+git add .gitignore
